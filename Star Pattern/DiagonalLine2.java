a8ac873a86046dc1e0ff6d3f86bcdd00d2d92c75
Md Saquib Alam <saquibalam934@gmail.com>
2024-07-17 18:00:26 +0530

    DiagonalLine2 Changes

--- Star Pattern/DiagonalLine2.java
+++ Star Pattern/DiagonalLine2.java
@@ -9,16 +9,13 @@ public class DiagonalLine2 {
     public static void main(String[] args) {
         
         for(int i=1;i<=5;i++){
-            for(int j=4;j>=i;j--){
-                System.out.print(" ");
-            }
-            for(int k=1;k<=i;k++){
-                if(i>=2 && k>1){
+            for(int j=1;j<=5;j++){
+                if( i+j==6){
+                    System.out.print("*");
+                }
+                else{
                     System.out.print(" ");
-                    }
-                    else{
-                        System.out.print("*");
-                    }
+                }
             }
             System.out.println();
         }
