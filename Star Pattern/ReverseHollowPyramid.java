c1d83d58758ec23848633a71a868a24ca7986db7
Md Saquib Alam <saquibalam934@gmail.com>
2024-07-17 15:20:26 +0530

    Reverse Hollow Pyramid

---
+++ Star Pattern/ReverseHollowPyramid.java
@@ -0,0 +1,24 @@
+// Write a program to print the following pattern.
+// *       *
+//  *     *
+//   *   *
+//    * *
+//     * 
+public class ReverseHollowPyramid {
+    public static void main(String[] args) {
+        for(int i=5;i>=1;i--){
+            for(int j=5;j>i;j--){
+                System.out.print(" ");
+            }
+            for(int k=1;k<(i*2);k++){
+                if(k>1 && k<(i*2)-1){
+                    System.out.print(" ");
+                }
+                else{
+                    System.out.print("*");
+                }
+            }
+            System.out.println(" ");
+        }
+    }
+}
